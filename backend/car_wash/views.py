1643a951ee912d2e68cf775081e164f4a7e3e424
AlexShonia <alex.shonia123@gmail.com>
2024-06-14 20:08:29 +0400

    refactoring

--- backend/car_wash/views.py
+++ backend/car_wash/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, logout, login
 from datetime import datetime
@@ -20,7 +20,6 @@ def your_appointments(request):
         return render(request, "car_wash/index.html", context)
     else:
         appointments = Appointment.objects.filter(user=request.user)
-        print(appointments[0].date)
         return render(
             request, "car_wash/your-appointments.html", {"appointments": appointments}
         )
@@ -44,31 +43,31 @@ def appointment(request):
 
 
 def choose_time(request):
-    # current_time = str(timezone.now()).split(" ")[1][:5]
-
     date = request.POST.get("date")
 
     request.session["date"] = date
     request.session["car_model"] = request.POST.get("car")
 
-    current_time = "9:00"
-    current_hour = int(current_time.split(":")[0])
-    current_min = int(current_time.split(":")[1])
+    starting_time = "8:30"
+    current_hour = int(starting_time.split(":")[0])
+    current_min = int(starting_time.split(":")[1])
     minute_increment = 30
 
-    times = [current_time]
+    times = []
 
     while current_hour <= 17:
-        if int(current_min) == 0:
-            current_min = minute_increment
+        current_min = int(current_min)
+        if current_min == 0:
+            current_min += minute_increment
         else:
             current_min = "00"
             current_hour += 1
-        current_time = str(current_hour) + ":" + str(current_min)
-        if Appointment.objects.filter(date=date + " " + current_time).exists():
+
+        time_block = str(current_hour) + ":" + str(current_min)
+        if Appointment.objects.filter(date=date + " " + time_block).exists():
             times.append("Reserved")
         else:
-            times.append(current_time)
+            times.append(time_block)
 
     return render(request, "car_wash/choose_time.html", {"times": times})
 
@@ -117,7 +116,6 @@ def login_view(request):
         try:
             u = User.objects.get(email=email)
         except User.DoesNotExist:
-            print("wrong")
             return render(
                 request,
                 "car_wash/login.html",
@@ -145,7 +143,6 @@ def register(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         repeat_password = request.POST.get("repeatPassword")
-        print(email, username, password, repeat_password)
         if password != repeat_password:
             return render(
                 request,
